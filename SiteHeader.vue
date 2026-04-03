e92f9f714f507b2c1b6b89de5d930fa66a0c8423
Rockson Kathar <151949464+PixelsWorker@users.noreply.github.com>
2026-04-03 14:01:21 +0530

    Stop click propagation for menu/backdrop; fix script syntax

---
+++ SiteHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="site-header" @click.stop>
+    <div class="top-bar" @click.stop>
+      <!-- Top bar content -->
+    </div>
+    <header @click.stop>
+      <!-- Header content -->
+      <button @click.stop="toggleMenu">Hamburger</button>
+      <submenu v-show="isVisible">
+        <!-- Submenu content -->
+      </submenu>
+    </header>
+  </div>
+</template>
+
+<script setup>
+// Removed stray brace and other script content
+
+</script>
